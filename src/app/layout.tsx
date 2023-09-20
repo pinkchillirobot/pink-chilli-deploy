316741e3b990c5ecce083ba6696ba97ceccda4cb
Siah <siah@goodwork.sg>
2023-09-20 14:31:38 +0800

    feat: meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,23 @@ import { Metadata } from "next";
 import { MenuButton } from "@/components/MenuButton";
 
 export const metadata: Metadata = {
-  title: "Pink Chilli in a Bowl",
-  description: "...",
+  title:
+    "Pink Chilli in a Bowl: A unique exploration of Singapore’s Culture, Food, and Architecture",
+  description:
+    "Discover a personal archive of forgotten places and time-honoured buildings in Singapore. Uncover hidden links between local establishments, architectural landmarks, and eateries.",
+  keywords:
+    "Singapore exploration, archive, forgotten places, iconic buildings, urban planning, design, cityscapes, food, eateries, architecture, landmarks, cultural diversity, street culture, Pink Chilli in a Bowl, Singapore eateries, hawkers, diversity, urbanism, travel, photography, cultural heritage, Swiss design, typology, Manuel Der Hagopian, Isabelle Der Hagopian, Mindy Tan, Don Wong, Stephanie Peh, Sarah Mineko Ichioka, Ming Tan, Adib Jalal, Ariel Lee, Practice Theory.",
+  openGraph: {
+    title:
+      "Pink Chilli in a Bowl: A unique exploration of Singapore’s Culture, Food, and Architecture",
+    description:
+      "Discover a personal archive of forgotten places and time-honoured buildings in Singapore. Uncover hidden links between local establishments, architectural landmarks, and eateries.",
+    images: `${
+      process.env.VERCEL_URL
+        ? `https:${process.env.VERCEL_URL}`
+        : "http://localhost:3000"
+    }/opengraph.png`,
+  },
 };
 
 export default function RootLayout({
--- src/components/LandingModule.tsx
+++ src/components/LandingModule.tsx
@@ -10,24 +10,24 @@ export const LandingModule = () => {
       className="relative w-full pb-2 border-chilli-grey border-x px-4"
     >
       <div className="h-[100vh] [&>span]:tracking-tighter [&>span]:leading-none relative">
-        <span className="block text-[29vh] absolute top-[0] left-[0] -indent-[0.07em]">
+        <span className="absolute block text-[23vw] top-0 left-0 lg:text-[29vh] lg:top-[0] lg:left-[0] -indent-[0.07em]">
           Pink
         </span>
-        <span className="block text-[29vh] absolute top-[25vh] right-[0]">
+        <span className="absolute block text-[23vw] top-[15vh] right-0 lg:text-[29vh] lg:top-[25vh] lg:right-[0]">
           Chilli
         </span>
-        <span className="block text-[29vh] absolute top-[50vh] left-[0] -indent-[0.07em] ">
+        <span className="absolute block text-[23vw] top-[45vh] left-0 lg:text-[29vh] lg:top-[50vh] lg:left-[0] -indent-[0.07em] ">
           in a
         </span>
-        <span className="block text-[29vh] absolute top-[50vh] left-[46vh]">
+        <span className="absolute block text-[23vw] top-[75vh] right-0 lg:text-[29vh] lg:top-[50vh] lg:left-[46vh]">
           Bowl
         </span>
 
-        <div className="block absolute right-0 top-0 h-[25vh] w-[25vh] bg-chilli-pink"></div>
-        <div className="block absolute left-0 top-[25vh] h-[20vh] w-[32vh] bg-chilli-pink"></div>
-        <div className="block absolute left-0 top-[75vh] h-[25vh] w-[20vh] bg-chilli-pink"></div>
-        <div className="block absolute left-[calc(20vh+1rem)] top-[75vh] h-[25vh] w-[20vh] bg-chilli-pink"></div>
-        <div className="block absolute right-0 top-[75vh] h-[25vh] w-[40vh] bg-chilli-pink"></div>
+        <div className="block absolute right-0 top-0 h-[25vh] w-[25vh] bg-chilli-pink/20"></div>
+        <div className="block absolute left-0 top-[25vh] h-[20vh] w-[32vh] bg-chilli-pink/20"></div>
+        <div className="block absolute left-0 top-[75vh] h-[25vh] w-[20vh] bg-chilli-pink/50"></div>
+        <div className="block absolute left-[calc(20vh+1rem)] top-[75vh] h-[25vh] w-[20vh] bg-chilli-pink/50"></div>
+        <div className="block absolute right-0 top-[75vh] h-[25vh] w-[40vh] bg-chilli-pink/50"></div>
       </div>
     </div>
   );
